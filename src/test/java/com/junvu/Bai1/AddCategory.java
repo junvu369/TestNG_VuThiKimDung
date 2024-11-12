1757840d0abdfb451dda1efbe2c27b0824f2775f
JunVu <kimdungcnc51.neu@gmail.com>
2024-11-13 01:08:32 +0700

    Upload code đầy đủ BT số 1 TestNG

--- src/test/java/com/junvu/Bai1/AddCategory.java
+++ src/test/java/com/junvu/Bai1/AddCategory.java
@@ -1,9 +1,11 @@
 package com.junvu.Bai1;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+import org.openqa.selenium.interactions.Actions;
 
 import java.time.Duration;
 
@@ -47,35 +49,57 @@ public class AddCategory {
 
         Thread.sleep(1000);
         driver.findElement(By.xpath("//input[@id='name']")).sendKeys("Giỏ quà bánh Tết");
+
         driver.findElement(By.xpath("//select[@name='parent_id']/following-sibling::button")).click();
         Thread.sleep(1000);
         driver.findElement(By.xpath("//select[@name='parent_id']/following-sibling::div//input")).sendKeys("giỏ bánh kẹo");
         Thread.sleep(1000);
         driver.findElement(By.xpath("(//select[@name='parent_id']/following-sibling::div//descendant::span)[1]")).click();
         Thread.sleep(1000);
+
         driver.findElement(By.xpath("//div/input[@id='order_level']")).sendKeys("123456");
         Thread.sleep(1000);
+
         driver.findElement(By.xpath("//select[@name='digital']/following-sibling::button")).click();
         Thread.sleep(1000);
-        driver.findElement(By.xpath("//select[@name='digital']/following-sibling::button/descendant::span[normalize-space()='Physical']")).click();
+        driver.findElement(By.xpath("(//select[@name='digital']/following-sibling::div//a)[1]")).click();
 
         driver.findElement(By.xpath("(//div/label[@for='signinSrEmail']/following-sibling::div//div[normalize-space()='Browse'])[1]")).click();
         Thread.sleep(2000);
         driver.findElement(By.xpath("//div[@id='aiz-select-file']/descendant::input[@name='aiz-uploader-search']")).sendKeys("Cosy");
-        driver.findElement(By.xpath("((//div[@id='aiz-select-file']/child::div)[2]/div)[1]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//div[(@title='Cosy.png') and (@data-value=2601)]")).click();
         driver.findElement(By.xpath("//div//button[normalize-space()='Add Files']")).click();
         Thread.sleep(1000);
 
-        driver.findElement(By.xpath("(//div/label[@for='signinSrEmail']/following-sibling::div//div[normalize-space()='Browse'])[2]")).click();
-        Thread.sleep(2000);
+        driver.findElement(By.xpath("(//div/label[@for='signinSrEmail']/following-sibling::div//div[normalize-space()='Browse'])[3]")).click();
         driver.findElement(By.xpath("//div[@id='aiz-select-file']/descendant::input[@name='aiz-uploader-search']")).sendKeys("hoa tuyết mai");
-        driver.findElement(By.xpath("(//div[@id='aiz-select-file']/child::div)[2]/div")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//div[(@title='hoa tuyết mai.png') and (@data-value=2618)]")).click();
         driver.findElement(By.xpath("//div//button[normalize-space()='Add Files']")).click();
         Thread.sleep(1000);
-        driver.findElement(By.xpath("//div/label[normalize-space()='Meta Title']/following-sibling::div/input")).sendKeys("ABC");
-        driver.findElement(By.xpath("//div/textarea[@name='meta_description']")).sendKeys("This is test");
-        //ĐANG Ở ĐÂY
 
+        driver.findElement(By.xpath("//div/label[normalize-space()='Meta Title']/following-sibling::div/input")).sendKeys("ABC");
+
+        driver.findElement(By.xpath("//div/textarea[@name='meta_description']")).sendKeys("This is test");
+        Thread.sleep(1000);
+
+        driver.findElement(By.xpath("//select[@name='filtering_attributes[]']/following-sibling::button")).click();
+        driver.findElement(By.xpath("//ul[@role='presentation']/descendant::a[@id='bs-select-3-0']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//select[@name='filtering_attributes[]']/following-sibling::button")).click();
+
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div/button[@type='submit']")).click();
+
+        //Kiểm tra lại category vừa add
+        driver.findElement(By.xpath("//div/input[@id='search']")).sendKeys("Giỏ quà bánh Tết");
+        Actions action = new Actions(driver);
+        action.sendKeys(Keys.ENTER).perform();
+
+        //Get text kết quả trường Name ở dòng đầu tiên
+        Thread.sleep(2000);
+        System.out.println(driver.findElement(By.xpath("(//div[normalize-space()='Categories']/following-sibling::div//td)[2]")).getText());
 
         //Close driver
         closeDriver();
